b59a7428ab5f82b9ecf558dacacb5e8bcdfcea24
Raul Alishov <alishov7394@gmail.com>
2026-05-13 12:44:51 +0400

    Container With Most Water

---
+++ src/Medium/ContainerWithMostWater.java
@@ -0,0 +1,18 @@
+package Medium;
+
+public class ContainerWithMostWater {
+	public int maxArea(int[] height) {
+		int left = 0;
+		int right = height.length - 1;
+		int maxArea = 0;
+		while (left < right) {
+			int width = right - left;
+
+			maxArea = Math.max(maxArea, Math.min(height[left], height[right]) * width);
+
+			if (height[left] < height[right]) left++;
+			else right--;
+		}
+		return maxArea;
+	}
+}
