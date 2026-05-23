d999d476b5e6a660723555b30491204360190f5d
Raul Alishov <alishov7394@gmail.com>
2026-05-23 19:27:27 +0400

    Reverse Linked List

---
+++ src/Easy/ReverseLinkedList.java
@@ -0,0 +1,27 @@
+package Easy;
+
+public class ReverseLinkedList {
+	public ListNode reverseList(ListNode head) {
+		ListNode temp = head;
+
+		ListNode after;
+		ListNode before = null;
+
+		while(temp != null) {
+			after = temp.next;
+			temp.next = before;
+			before = temp;
+			temp = after;
+		}
+
+		return before;
+	}
+}
+
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
