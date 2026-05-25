262b745aa8a86573a49d3dd88704b8fb37d287b7
Raul Alishov <alishov7394@gmail.com>
2026-05-25 21:57:58 +0400

    Remove Nth Node From End of List

--- src/Easy/ReverseLinkedList.java
+++ src/Easy/ReverseLinkedList.java
@@ -18,9 +18,9 @@ public class ReverseLinkedList {
 	}
 }
 
-class ListNode {
+public class ListNode {
      int val;
-     ListNode next;
+     public ListNode next;
      ListNode() {}
      ListNode(int val) { this.val = val; }
      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
