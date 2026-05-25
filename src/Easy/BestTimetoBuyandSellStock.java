7e319595c97d8e48483d6ff0e1bd67e20df76b07
Raul Alishov <alishov7394@gmail.com>
2026-05-25 21:59:23 +0400

    Best Time to Buy and Sell Stock

---
+++ src/Easy/BestTimetoBuyandSellStock.java
@@ -0,0 +1,14 @@
+package Easy;
+
+public class BestTimetoBuyandSellStock {
+	public int maxProfit(int[] prices) {
+		int maxP = 0;
+		int minBuy = prices[0];
+
+		for (int sell : prices) {
+			maxP = Math.max(maxP, sell - minBuy);
+			minBuy = Math.min(minBuy, sell);
+		}
+		return maxP;
+	}
+}
