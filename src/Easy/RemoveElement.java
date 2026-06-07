52b24b9ca69b46065f1d12f35b29e79ce1ba4367
Alishov Raul <alishov7394@gmail.com>
2026-06-07 17:48:13 +0400

    Remove Element

---
+++ src/Easy/RemoveElement.java
@@ -0,0 +1,15 @@
+package Easy;
+
+public class RemoveElement {
+	public int removeElement(int[] nums, int val) {
+		int index = 0;
+
+		for (int num: nums) {
+			if (num != val) {
+				nums[index++] = num;
+			}
+		}
+
+		return index;
+	}
+}
